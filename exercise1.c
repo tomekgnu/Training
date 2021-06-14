6e3ca5f93d0c8da52c05396596ea7d90834947a1
Tomasz Urbaniak <tomasz.urbaniak@wzl2.mil.pl>
2021-06-14 07:18:17 +0200

    1) Pierwsze cwiczenie w C

---
+++ exercise1.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+// 1. Definicja funkcji main: tu zaczyna dzialac program
+int main(void){
+
+	int result;	// zmienna-rezultat dzialan
+	
+	result = add(2,2);	// dodawanie
+	showNumber(result);	// wynik=4
+	
+	result = subtract(4,3);	// odejmowanie
+	showNumber(result);		// wynik=1
+	
+	result = multiply(2,2);	// mnozenie
+	showNumber(result);		// wynik=4
+	
+	result = divide(4,2);		// dzielenie
+	showNumber(result);		// wynik=2
+
+}
+
+// 2. Definicja funkcji dodajacej 2 liczby
+int add(int a,int b){
+
+
+	return a + b;
+
+
+}
+
+// 3. Definicja funkcji odejmujacej
+// 4. Definicja funkcji mnozacej
+// 5. Definicja funkcji dzielacej
+
+void showNumber(int n){
+	
+	printf("%d\n",n);
+}
